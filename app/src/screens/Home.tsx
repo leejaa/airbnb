043625b24cc52b9132369616a6b2c0a2d01d7c90
이자훈 <leejahun0@gmail.com>
2019-12-31 17:17:44 +0900

    하트 모양추가 및 카드 2분할

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity, Dimensions, Image, ScrollView, FlatList } from 'react-native';
 import { NavigationStackScreenProps } from "react-navigation-stack";
 import { SearchBar } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
 import _ from 'lodash';
 const uuidv1 = require('uuid/v1');
 import Carousel from '../components/Carousel';
@@ -53,7 +54,8 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderWidth: 0.5,
     borderColor: '#d6d7da',
-    margin: 20,
+    width: '45%',
+    marginLeft: 15,
   },
   CardTitle: {
     width: '100%',
@@ -69,31 +71,45 @@ const styles = StyleSheet.create({
 });
 
 export const Home: React.FC<NavigationStackScreenProps> = ({ navigation }) => {
-  const [ list, setList ] = useState(data);
-  const [ page, setPage ] = useState(5);
+  const [list, setList] = useState(data);
+  const [page, setPage] = useState(5);
 
   return (
     <View>
       <Carousel />
       <View>
         {
-            <FlatList
-              keyExtractor={ item => item.id }
-              data={ list.slice( 0, page ) }
-              onEndReachedThreshold={ 0.1 }
-              onEndReached={ () => {
-                setPage( page + 5 );
-              }}
-              renderItem={ ({ item }) => {
-                return (
+          <FlatList
+            keyExtractor={item => item.id}
+            data={list.slice(0, page)}
+            onEndReachedThreshold={0.1}
+            onEndReached={() => {
+              setPage(page + 5);
+            }}
+            renderItem={({ item }) => {
+              return (
+                <View style={ { flex: 1, flexDirection: 'row', marginTop: 20 } }>
                   <View style={styles.CardContainer}>
-                    <Image source={{ uri: item.uri }} style={{ width: "100%", height: 200, borderRadius: 4 }}/>
+                    <View style={ { position: 'absolute', right: 0, zIndex: 1 } }>
+                      <Ionicons name="md-heart-empty" size={16} color="red" />
+                    </View>
+                    <Image source={{ uri: item.uri }} style={{ width: "100%", height: 200, borderRadius: 4 }} />
                     <Text style={styles.CardTitle}>{item.title}</Text>
                     <Text style={styles.CardContent}>{item.content}</Text>
                   </View>
+
+                  <View style={styles.CardContainer}>
+                    <View style={ { position: 'absolute', right: 0, zIndex: 1 } }>
+                      <Ionicons name="md-heart-empty" size={16} color="red" />
+                    </View>
+                    <Image source={{ uri: item.uri }} style={{ width: "100%", height: 200, borderRadius: 4 }} />
+                    <Text style={styles.CardTitle}>{item.title}</Text>
+                    <Text style={styles.CardContent}>{item.content}</Text>
+                  </View>
+                </View>
                 );
-              }}
-            />
+            }}
+          />
         }
       </View>
     </View>
